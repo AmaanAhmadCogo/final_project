992663bc03c479963a427be09365092f875d809c
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2023-02-08 00:49:59 +0530

    css changed, added footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,47 +4,47 @@ import './Home.css';
 import { BiChevronDown } from "react-icons/bi"
 
 function Home() {
-  
+
     return (
         <>
             {/* header    */}
             {/* bold heading ?  */}
-            <div style={styles.header}>
+            <div className='header'>
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
-                    <div style={styles.logo}><img style={{ width: "40px" }} src="https://cdn0.iconfinder.com/data/icons/social-networks-and-media-flat-icons/136/Social_Media_Socialmedia_network_share_socialnetwork_network-22-512.png" /></div>
+                    <div className='logo'><img style={{ width: "40px" }} src="https://cdn0.iconfinder.com/data/icons/social-networks-and-media-flat-icons/136/Social_Media_Socialmedia_network_share_socialnetwork_network-22-512.png" /></div>
                     <div><p style={{ fontWeight: "bold", fontSize: "20px" }}>BlogStar</p></div>
                 </div>
-                <div style={styles.headerStack}>
+                <div className='headerStack'>
                     <div>Home</div>
-                    <div style={styles.pr_re}>
+                    <div className='pr_re'>
                         Create Blog
                         <BiChevronDown fill={"#475467"} />
                     </div>
-                    <div style={styles.pr_re}>
+                    <div className='pr_re'>
                         Search Blog
                         <BiChevronDown fill={"#475467"} />
                     </div>
                     <div>Pricing</div>
                 </div>
-                <div style={styles.log}>
-                    <button style={styles.login}>Log in</button>
-                    <button style={styles.signup}>Sign up</button>
+                <div className='log'>
+                    <button className='login'>Log in</button>
+                    <button className='signup'>Sign up</button>
                 </div>
             </div>
 
             {/*  Stories and Interviews   // blog heading*/}
-            <div style={styles.blogHead}>
-                <div style={styles.leftBlogHead}>
+            <div className='blogHead'>
+                <div className='leftBlogHead'>
                     <div style={{ fontSize: "12px", color: "#7F56D9" }}>Our Blog</div>
                     <div style={{ color: "black" }}><h1>Stories and Interviews</h1></div>
                     <br />
                     <div>
                         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
                             <div>
-                                <input style={styles.emailInput} placeholder='Enter your email' />
+                                <input className='emailInput' placeholder='Enter your email' />
                             </div>
                             <div>
-                                <button style={styles.signup}>Get Started</button>
+                                <button className='signup' >Get Started</button>
                             </div>
                         </div>
                         <br />
@@ -52,40 +52,152 @@ function Home() {
                     </div>
 
                 </div>
-                <div style={styles.supportingText}>
+                <div className='supportingText'>
                     <p>Subscribe to learn about new product features, the latest in technology, solutions, and updates.</p>
                 </div>
             </div>
 
             {/* Blogs */}
-            <div style={styles.blogArea}>
+            <div className='blogArea'>
 
                 {/* buttons to view by category */}
-                <div style={styles.buttonSpace}>
-                    <button style={styles.categoryBtn} className="btnP">View All</button>
-                    <button style={styles.categoryBtn} className="btnP">Design</button>
-                    <button style={styles.categoryBtn} className="btnP">Product</button>
-                    <button style={styles.categoryBtn} className="btnP">Software Development</button>
-                    <button style={styles.categoryBtn} className="btnP">Customer Services</button>
+                <div className='buttonSpace'>
+                    <button className="btnP">View All</button>
+                    <button className="btnP">Design</button>
+                    <button className="btnP">Product</button>
+                    <button className="btnP">Software Development</button>
+                    <button className="btnP">Customer Services</button>
                 </div>
 
-                <div style={styles.blogs}>
-                    <div style={styles.blogRow}>
-                        <div style={styles.blog}>
-                            <div style={styles.blogImageDiv}><img style={styles.blogImage} src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg"/></div>
-                            <div style={styles.blogContent}>
-                                Article Category
-                                <h6>Article Title</h6>
-                                <p>lorem hioivhjwoijvwpoijvowivjpqovjpwov</p>
-                                <p>Blogger's Name</p>
-                                <p>Publishing Date</p>
+                <div className='blogs'>
+                    <div className='blogRow'>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6 style={{color:"#7F56D9"}}>Article Category</h6>
+                                <p>Blog 1</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
                             </div>
                         </div>
-                        <div style={styles.blog}>2nd blog</div>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6 style={{color:"#7F56D9"}}>Article Category</h6>
+                                <p>Blog 2</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div style={styles.blogRow}>
-                        <div style={styles.blog}>3rd blog</div>
-                        <div style={styles.blog}>4th blog</div>
+
+                    <div className='blogRow'>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6 style={{color:"#7F56D9"}}>Article Category</h6>
+                                <p>Blog 1</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6 style={{color:"#7F56D9"}}>Article Category</h6>
+                                <p>Blog 2</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='blogRow'>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6>Article Category</h6>
+                                <p>Blog 1</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='blog'>
+                            <div className='blogImageDiv' ><img className='blogImage' src="https://studyonline.rmit.edu.au/sites/default/files/RMIT_MHRM_Skyscraper_Article_%232_-Creating_and_Sustaining_a_Positive_Workplace_Culture_Header_0.jpg" /></div>
+                            <div className='blogContent'>
+                                <h6>Article Category</h6>
+                                <p>Blog 2</p>
+                                <h5>lorem hioivhjwoijvwpoijvowivjpqovjpwov</h5>
+                                <div>
+                                    <h6>Blogger's Name</h6>
+                                    <h6>Publishing Date</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* freeTrial */}
+            <div className='freeTrial'>
+                <div><h1>Start your free trial</h1></div>
+                <div><p style={{ color: "#475467" }}>Join over 4,000+ startups already growing with Untitled.</p></div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <div><button className='learnMorebtn'>Learn More</button></div>
+                    <div><button className='signup'>Get Started</button></div>
+                </div>
+            </div>
+
+            {/* footer */}
+            <div className='footer'>
+                <div className='upperFooter'>
+                    <div className='footerColumn'>
+                        <div>Product</div>
+                        <div>Overview</div>
+                        <div>Features</div>
+                        <div>Solutions</div>
+                        <div>Pricing</div>
+                    </div>
+                    <div className='footerColumn'>
+                        <div>Company</div>
+                        <div>About us</div>
+                        <div>Careers</div>
+                        <div>Press</div>
+                        <div>Contact</div>
+                    </div>
+                    <div className='footerColumn'>
+                        <div>Product</div>
+                        <div>Overview</div>
+                        <div>Features</div>
+                        <div>Solutions</div>
+                        <div>Pricing</div>
+                    </div>
+                    <div className='footerColumn'>
+                        <div>Company</div>
+                        <div>About us</div>
+                        <div>Careers</div>
+                        <div>Press</div>
+                        <div>Contact</div>
+                    </div>
+                </div>
+                <div className='lowerFooter'>
+                    <div style={{ padding: "0 50px" }}>
+                        <p>© 2077 Untitled UI. All rights reserved.</p>
                     </div>
                 </div>
             </div>
@@ -94,162 +206,3 @@ function Home() {
 }
 
 export default Home;
-  
-const styles = {
-    // header ---------------------------------------------------
-    header: {
-        boxSizing: "border-box",
-        height: "80px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "0 70px",
-        borderBottom: "2px groove #f0f2f5",
-    },
-    headerStack: {
-        display: "flex",
-        justifyContent: "space-evenly",
-        width: "50%",
-        color: "#475467",
-        // fontWeight:"600"
-    },
-    logo: {
-        width: "44px",
-        height: "44px",
-    },
-    pr_re: {
-        display: "flex",
-        alignItems: "center",
-    },
-    log: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "10px 18px",
-        gap: "6px",
-    },
-    login: {
-        width: "95px",
-        height: "44px",
-        fontFamily: 'Inter',
-        fontstyle: "normal",
-        fontweight: "600",
-        fontsize: "16px",
-        lineheight: "24px",
-        // color: "#475467",
-        backgroundColor: "white",
-        border: "0",
-    },
-    signup: {
-        width: "100px",
-        height: "44px",
-        fontFamily: 'Inter',
-        fonStyle: "normal",
-        fontWeight: "600",
-        fontSize: "16px",
-        lineHeight: "24px",
-        background: "#7F56D9",
-        border: "1px solid #7F56D9",
-        boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
-        borderRadius: "8px",
-        color: "white",
-    },
-    // -------------------------- header ends here
-
-    // Stories and Interviews-------------------------------
-    blogHead: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        // height: "402px",
-        width: "100%",
-        color: "#546278",
-        // backgroundColor: "green",
-        padding: "50px 200px"
-    },
-    leftBlogHead: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        height: "200px",
-        // backgroundColor: "aqua",
-        padding: "10px 100px 10px 0"
-    },
-    supportingText: {
-        // width: "384px",
-        width: "35%",
-        height: "90px",
-        fontSize: "20px",
-        // backgroundColor: "yellow"
-    },
-    emailInput: {
-        height: "44px",
-        fontFamily: 'Inter',
-        fonStyle: "normal",
-        fontWeight: "600",
-        fontSize: "16px",
-        lineHeight: "24px",
-        // borderColor: "#f0f2f5",
-        borderRadius: "8px",
-        width: "100%",
-        paddingLeft: "10px"
-    },
-
-    // Blog buttons to view by category---------------------------------------------
-    blogArea: {
-        padding: "10px 80px",
-    },
-    buttonSpace: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        width: "50%"
-    },
-    categoryBtn: {
-        padding: "6px",
-        border: 0,
-        borderRadius: "4px"
-    },
-
-    // Blogs --------------------------------------------------------------------
-    blogs: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-    },
-    blogRow: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        padding: "20px 20px 20px 0"
-    },
-    blog: {
-        display: "flex",
-        justifyContent: "flex-start",
-        // alignItems:"flex-start",
-        backgroundColor: "green",
-        width: "48%",
-        height: "200px",
-    },
-    blogImageDiv: {
-        // width: "320px",
-        // height: "200px",
-    },
-    blogImage:{
-        width: "320px",
-        height: "200px",
-        objectFit:"contain"
-    },
-    blogContent:{
-        display:"flex",
-        flexDirection:"column",
-
-    },
-    bloggerInfo:{
-
-    }
-}
